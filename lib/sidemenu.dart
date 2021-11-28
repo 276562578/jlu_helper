1d9e97174f3c459ce583f4611ea592ac8b67d834
276562578 <276562578@163.com>
2021-11-28 23:04:57 +0800

    支持windows，初步设计sidebar布局

--- lib/sidemenu.dart
+++ lib/sidemenu.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-class SideMenu extends StatelessWidget{
+class SideMenu extends StatelessWidget {
+  bool isLogin = false;
+  String name = "";
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(color: Colors.blue,);
+    return Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.lightBlueAccent,
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: isLogin
+                    ? [Icon(Icons.account_circle), Expanded(child: Text(name)),IconButton(onPressed: (){}, icon: Icon(Icons.exit_to_app))]
+                    : [Container(child: Row(children:[Icon(Icons.account_circle_outlined), Expanded(child: Text("未登录"))]),)],
+              ),
+            )
+          ],
+        ));
   }
-
 }
