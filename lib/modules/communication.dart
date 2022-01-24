1bca33733f4e533bf532217e0f2cec998db2171b
276562578 <276562578@163.com>
2022-01-24 11:32:38 +0800

    适配异形屏

--- lib/modules/communication.dart
+++ lib/modules/communication.dart
@@ -5,22 +5,24 @@ class Communication extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              if (Responsive.isMobile(context))
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: Icon(Icons.arrow_back))
-                  ],
-                ),
-              Text("Communication page")
-            ],
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                if (Responsive.isMobile(context))
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(Icons.arrow_back))
+                    ],
+                  ),
+                Text("Communication page")
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/modules/oa_new.dart
+++ lib/modules/oa_new.dart
@@ -8,13 +8,12 @@ class OANews extends StatelessWidget {
   final GlobalKey<ScaffoldState> oa_new_key = GlobalKey<ScaffoldState>();
 
   Future<List<ListTile>> getOANews() async {
-    final response =
-        await get(Uri.parse(oa_news_url));
+    final response = await get(Uri.parse(oa_news_url));
     var document = parse(response.body);
     return document.getElementsByClassName("li rel").map((element) {
       String title = element.getElementsByClassName("font14")[0].text;
       String author = element.getElementsByClassName("column")[0].innerHtml;
-      String time = element.getElementsByClassName("time")[0].innerHtml.replaceAll('&nbsp;', ' ');
+      String time = element.getElementsByClassName("time")[0].innerHtml.replaceAll('&nbsp;', '');
       ;
       return ListTile(
         title: Text(title),
@@ -28,26 +27,30 @@ class OANews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       key: oa_new_key,
-      body: Column(children: [
-        if (Responsive.isMobile(context))
-          Row(
-            children: [IconButton(onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back))],
+      body: SafeArea(
+        child: Column(children: [
+          if (Responsive.isMobile(context))
+            Row(
+              children: [
+                IconButton(onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back)),
+                Text("OA新闻")
+              ],
+            ),
+          Expanded(
+            child: FutureBuilder<List<ListTile>>(
+                future: getOANews(),
+                // initialData: [],
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+                  if (snapshot.error != null) return Center(child: Text("请求失败"));
+                  final data = snapshot.data;
+                  return ListView(
+                    children: data!,
+                  );
+                }),
           ),
-        Expanded(
-          child: FutureBuilder<List<ListTile>>(
-              future: getOANews(),
-              initialData: [],
-              builder: (context, snapshot) {
-                // if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
-                // if (snapshot.error != null) return Center(child: Text("请求失败"));
-                final data = snapshot.data;
-                return ListView(
-                  shrinkWrap: true,
-                  children: data!,
-                );
-              }),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
